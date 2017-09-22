f75a118476bb801ab636f3bb9c9367284e5dbbdc
Quang C. Huynh <qhuynh@vims.edu>
2017-09-21 23:08:54 -0400

    Add 2 minor functions.

--- R/internal_misc.R
+++ R/internal_misc.R
@@ -10,6 +10,15 @@ bin_length.Len_summary <- function(x, y) {
   data.frame(Year = rep(x, length(y$mids)), Length = y$mids, Frequency = y$counts)
 }
 
-returnMLCRloglike <- function(x) {
-  if(inherits(x, "try-error")) as.numeric(rep(NA, 2)) else x$report()$loglike
+get_M_MSM1S <- function(MLZ_data) {
+  if(length(MLZ_data@M) > 0) M <- MLZ_data@M
+  else M <- 0.01
+  return(M)
+}
+
+produce_MLmulti_warnings <- function(Z, Z.limit) {
+  warning.flag <- logical(length = length(Z.limit))
+  for(i in 1:length(Z.limit)) warning.flag[i] <- any(Z[, i] <= Z.limit[i])
+  if(any(warning.flag)) warning("There are mortality estimates at boundary (Z = 0.01 or M).")
+  invisible()
 }
